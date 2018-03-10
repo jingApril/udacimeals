0056071add3c7f5e64f8223d1521420945ffd14c
jingApril <kady762@163.com>
2018-03-10 18:54:07 +0800

    message

--- src/components/App.js
+++ src/components/App.js
@@ -16,13 +16,15 @@ class App extends Component {
     )
   }
 }
-function mapStateToProps(calendar) {
+function mapStateToProps({calendar, food}) {
     const dayOrder =['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
     return{
         calendar:dayOrder.map((day) => ({
             day,
             meals: Object.keys(calendar[day]).reduce((meals, meal) => {
-                meals[meal] = calendar[day][meal] ? calendar[day][meal] : null
+                meals[meal] = calendar[day][meal]
+                ?food[calendar[day][meal]]
+                : null
                 return meals
             },{})
         }))
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,23 @@
+import { combineReducers } from 'redux'
 import {
 	ADD_RECIPE,
 	REMOVE_FROM_CALENDAR,
 } from '../actions'
 
+
+ function food (state ={}, action) {
+	switch(action.type) {
+		case ADD_RECIPE:
+			const {recipe} =action
+
+			return {
+				...state,
+				[recipe.label]: recipe,
+			}
+		default:
+			return state
+	}
+ }
 const initialCalendarState = {
 	sunday: {
 		breakfast: null,
@@ -69,4 +84,7 @@ function calendar (state = initialCalendarState, action) {
 	}
 }
 
-export default calendar
+export default combineReducers({
+	food,
+	calendar,
+})
